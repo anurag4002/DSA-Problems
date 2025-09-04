275d2ebefca9b918d6b970f3e3f501a93565539d
anurag4002 <mishraanurag66031@gamil.com>
2025-09-04 23:21:38 +0530

    Check if Array is Sorted

---
+++ CheckIfArraySorted.java
@@ -0,0 +1,22 @@
+public class CheckIfArraySorted{
+
+     public boolean check(int[] nums) {
+        int peak = 0;
+
+        int n = nums.length;
+
+        for(int i = 0; i < n ; i++){
+            if(nums[i] > nums[(i + 1) % n]){
+                peak++;
+            }
+        }
+
+        if(peak > 1){
+            return false;
+        }
+        
+        return true;
+    }
+}
+    
+
