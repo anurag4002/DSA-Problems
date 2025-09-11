4e2cd4c4d18d781757961ff9503027b223ac58c9
Anurag mishra <112270728+anurag4002@users.noreply.github.com>
2025-09-12 05:11:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/C++ Input / Output/c-input-output.java
@@ -0,0 +1,10 @@
+// User function Template for Java
+class Solution {
+    static int multiplication(int A, int B) {
+       
+       int product = A *B ;
+          return product;
+       
+    }
+  
+}
