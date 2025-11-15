f6db4c721505e3e47d62e9c6058ff0eecfde188e
Anurag mishra <112270728+anurag4002@users.noreply.github.com>
2025-11-15 16:27:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Binary Search/binary-search.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int binarysearch(int[] arr, int k) {
+        int start = 0, end = arr.length - 1;
+        int ans = -1;
+
+        while(start <= end) {
+            int mid = (start + end) / 2;
+
+            if(arr[mid] == k) {
+                ans = mid;
+                end = mid - 1;   
+            }
+            else if(arr[mid] < k) {
+                start = mid + 1;
+            }
+            else {
+                end = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
